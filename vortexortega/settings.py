93a7843b75a5c01ad0610b0099d4236cd653d3cf
krsnagovinda <122837090+krsnagovinda@users.noreply.github.com>
2023-01-21 00:16:26 -0600

    Update settings.py

--- vortexortega/settings.py
+++ vortexortega/settings.py
@@ -80,25 +80,12 @@ WSGI_APPLICATION = 'vortexortega.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
    'default': {
-      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vortexortega_1_2',
-        'USER': 'krsna',
-        'PASSWORD': 'deutschland123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-#DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 
 # Password validation
